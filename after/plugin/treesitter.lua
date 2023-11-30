70b18bae02e8d8fb8326688b5c127e2a8d1eec2d
Robert Brunhage <robertbrunhage@gmail.com>
2023-11-30 18:29:29 +0100

    chore: no more quickfix pls

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -24,12 +24,12 @@ require("nvim-treesitter.configs").setup({
 	},
 })
 
--- Remove this once https://github.com/nvim-treesitter/nvim-treesitter/issues/4945 is fixed
-local parser = require("nvim-treesitter.parsers").get_parser_configs()
-parser.dart = {
-	install_info = {
-		url = "https://github.com/UserNobody14/tree-sitter-dart",
-		files = { "src/parser.c", "src/scanner.c" },
-		revision = "8aa8ab977647da2d4dcfb8c4726341bee26fbce4", -- The last commit before the snail speed
-	},
-}
+-- -- Remove this once https://github.com/nvim-treesitter/nvim-treesitter/issues/4945 is fixed
+-- local parser = require("nvim-treesitter.parsers").get_parser_configs()
+-- parser.dart = {
+-- 	install_info = {
+-- 		url = "https://github.com/UserNobody14/tree-sitter-dart",
+-- 		files = { "src/parser.c", "src/scanner.c" },
+-- 		revision = "8aa8ab977647da2d4dcfb8c4726341bee26fbce4", -- The last commit before the snail speed
+-- 	},
+-- }
